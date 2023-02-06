ec7b4a2779dd7ea06cf9f26b48184274a994aeac
SBahrmann <s.bahrmann@gmail.com>
2023-02-06 08:54:35 +0100

    add accent color

---
+++ src/app/css/accent-color/accent-color.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accent-color',
+  templateUrl: './accent-color.component.html',
+  styleUrls: ['./accent-color.component.scss']
+})
+export class AccentColorComponent {
+
+}
