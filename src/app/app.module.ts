b5bba8e2faec367947ed819c900f4563bc67ff11
SBahrmann <s.bahrmann@gmail.com>
2023-02-06 08:59:03 +0100

    add anchor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { AccentColorComponent } from './css/accent-color/accent-color.component';
 import { AllUnsetComponent } from './css/all-unset/all-unset.component';
+import { AnchorComponent } from './css/anchor/anchor.component';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
@@ -11,6 +12,7 @@ import { ApiWebAnimationComponent } from './js/api-web-animation/api-web-animati
     AppComponent,
     AllUnsetComponent,
     AccentColorComponent,
+    AnchorComponent,
     ApiWebAnimationComponent
   ],
   imports: [
